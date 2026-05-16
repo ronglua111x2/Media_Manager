306ebb0e3c93ebc0aa88d6a55cfe7b235dd58714
ronglua111x2 <baorong2003@gmail.com>
2026-05-16 09:45:13 +0700

    Add initial project structure and configuration for Media Manager app

---
+++ media management app/media management app/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace media_management_app
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
